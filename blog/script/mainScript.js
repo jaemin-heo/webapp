e436e3de9496d1a95b2daf529e7b43d923ae6109
DESKTOP-KC8MD42\user1 <mtyu02345@gmail.com>
2025-04-10 08:58:24 +0900

    test

--- blog/script/mainScript.js
+++ blog/script/mainScript.js
@@ -11,5 +11,4 @@ function inquirySumbit(){
 function mainTagLoad(){
     $("textarea[name='inquiryText']").val($.cookie('inquiryText'));
     //위 내용은 main.html에서 생성됬으므로 main.html내에 mainScript.js를 통해서만 pageLoad가 가능함
-    //이 js에서 pageLoad했을 경우 이미 index.html 생성된 값만 수정 가능
 }
